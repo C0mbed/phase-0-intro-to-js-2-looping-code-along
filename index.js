cae7f297791659d8a519e1ef09aa26d025a094b8
C0mbed <matt@farncombe.co>
2021-12-16 14:08:55 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(arr) {
+    const newArr = [];
+    arr.forEach(element => newArr.push("Thank you, " + element + ", for the wonderful surprise gift!"));
+    return newArr;
+}
+
+function countDown(num) {
+    let count = num;
+    while (count > -1) {
+        console.log(count);
+        count--;
+    }
+}
